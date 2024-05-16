32b0095a01aa3c8deb5cfc27acf6511b24ec9007
Roy Eyal <roy.eyal@gmail.com>
2024-05-16 17:28:07 +0300

    Update berelevantai.js

--- berelevantai.js
+++ berelevantai.js
@@ -91,7 +91,7 @@ document
 	.getElementById("button-00-bottom")
 	.addEventListener("click", function () {
 		var select = document.getElementById("select-course");
-		select.value = "Devs"; // Change the selected option to value 2
+		select.value = "Code"; // Change the selected option to value 2
 		select.dispatchEvent(new Event("change")); // Trigger the change event
 		new_titleRegister.innerHTML = aiCourses[0];
 	});
